d1f1cc64109bb2ee42f7caae55a855ee07ef9b91
aandrejbur <arburenkov@gmail.com>
2017-01-27 16:35:04 +0300

    Initial Commit

---
+++ Log_file_reader/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Log_file_reader
+//
+//  Created by Андрей Буренков on 27.01.17.
+//  Copyright © 2017 Андрей Буренков. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
